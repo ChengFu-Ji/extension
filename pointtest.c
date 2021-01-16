565097260729b939b4edfaad80b8bec8f386d8a4
sidney-Hung <sidney35792461@gmail.com>
2021-01-16 17:30:11 +0800

    alreay_change

--- pointtest.c
+++ pointtest.c
@@ -1,13 +1,21 @@
-#include<stdio.h>
+#include<stdio.h> 
 #include<stdlib.h>
 
-int main(){
-    char *i;
-    i=(char *)malloc(1000);
+int main()
+{
+    char *i,*y;
+	char a[5]="what"; 
+	int z=0;
 
-    char a[10]="LOL";
-    i=a
-
-    printf("%s\n",i);
-    
-return 0;}
+	i=(char *)malloc(1000);	
+	y=i;
+	
+	for( z ; z<5 ; z++)
+	{
+		*y=a[z];
+		y++;
+	}
+	
+	printf("%s\n",i);   
+	return 0;
+}
