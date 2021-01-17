98b7bdc37c3d0f01bf92af6dc8f3dcabb39d7246
sidney-Hung <sidney35792461@gmail.com>
2021-01-17 09:52:46 +0800

    use NULL of the while loop

--- pointtest.c
+++ pointtest.c
@@ -2,18 +2,19 @@
 #include<stdlib.h>
 
 int main()
-{   
+{
     char *i,*y;
-	char a[5]="what"; 
+	char a[]="what"; 
 	int z=0;
 
 	i=(char *)malloc(1000);	
 	y=i;
 	
-	for( z ; z<5 ; z++)
+	while(a[z]!='\0')	
 	{
 		*y=a[z];
 		y++;
+		z++;
 	}
 	
 	printf("%s\n",i);   
